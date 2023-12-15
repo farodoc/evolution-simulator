3c8dbee7162a28ec4899206c07c7fac622220dbe
Jakub Konopka <jakubkonopka@student.agh.edu.pl>
2023-12-15 18:21:19 +0100

    odchudzenie kodu
    stałe bordery

--- oolab/src/main/java/agh/ics/oop/model/GrassField.java
+++ oolab/src/main/java/agh/ics/oop/model/GrassField.java
@@ -8,7 +8,8 @@ import java.util.Map;
 import static java.lang.Math.sqrt;
 
 public class GrassField extends AbstractWorldMap{
-    private final int grassNumber;
+    private int mapSize = 10;
+    private int grassNumber;
     public GrassField(int grassNumber) {
         super();
         this.grassNumber = grassNumber;
@@ -16,10 +17,8 @@ public class GrassField extends AbstractWorldMap{
     }
 
     private final Map<Vector2d, Grass> grassTiles = new HashMap<>();
-    private Vector2d DYNAMIC_BOTTOM_LEFT_BORDER = new Vector2d(Integer.MAX_VALUE,Integer.MAX_VALUE);
-    private Vector2d DYNAMIC_TOP_RIGHT_BORDER = new Vector2d(Integer.MIN_VALUE,Integer.MIN_VALUE);
-    private Vector2d BOTTOM_LEFT_GRASS;
-    private Vector2d TOP_RIGHT_GRASS;
+    private Vector2d BOTTOM_LEFT_MAP_BORDER = new Vector2d(0,0);
+    private Vector2d TOP_RIGHT_MAP_BORDER = new Vector2d(mapSize - 1,mapSize - 1);
 
     private void generateGrassTiles(){
         int cords = (int) sqrt(grassNumber * 10);
@@ -30,14 +29,10 @@ public class GrassField extends AbstractWorldMap{
             Vector2d newGrassPosition = new Vector2d(x,y);
             Grass newGrass = new Grass(newGrassPosition);
             if(!grassTiles.containsKey(newGrassPosition)){
-                DYNAMIC_TOP_RIGHT_BORDER = DYNAMIC_TOP_RIGHT_BORDER.upperRight(newGrassPosition);
-                DYNAMIC_BOTTOM_LEFT_BORDER = DYNAMIC_BOTTOM_LEFT_BORDER.lowerLeft(newGrassPosition);
                 grassTiles.put(newGrassPosition,newGrass);
                 cnt++;
             }
         }
-        BOTTOM_LEFT_GRASS = DYNAMIC_BOTTOM_LEFT_BORDER;
-        TOP_RIGHT_GRASS = DYNAMIC_TOP_RIGHT_BORDER;
     }
 
     Map<Vector2d, Animal> getAnimals() {
@@ -57,17 +52,6 @@ public class GrassField extends AbstractWorldMap{
         return grassTiles.getOrDefault(position, null);
     }
 
-    private void findBorders(){
-        DYNAMIC_BOTTOM_LEFT_BORDER = BOTTOM_LEFT_GRASS;
-        DYNAMIC_TOP_RIGHT_BORDER = TOP_RIGHT_GRASS;
-
-        for(Map.Entry<Vector2d, Animal> entry : animals.entrySet()){
-            Vector2d key = entry.getKey();
-            DYNAMIC_BOTTOM_LEFT_BORDER = DYNAMIC_BOTTOM_LEFT_BORDER.lowerLeft(key);
-            DYNAMIC_TOP_RIGHT_BORDER = DYNAMIC_TOP_RIGHT_BORDER.upperRight(key);
-        }
-    }
-
     @Override
     public ArrayList<WorldElement> getElements(){
         ArrayList<WorldElement> result = super.getElements();
@@ -77,7 +61,6 @@ public class GrassField extends AbstractWorldMap{
 
     @Override
     public Boundary getCurrentBounds(){
-        findBorders();
-        return new Boundary(DYNAMIC_BOTTOM_LEFT_BORDER, DYNAMIC_TOP_RIGHT_BORDER);
+        return new Boundary(BOTTOM_LEFT_MAP_BORDER, TOP_RIGHT_MAP_BORDER);
     }
 }
