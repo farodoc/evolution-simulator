8aaa91950d25dce657f042e6e8456b0ad863e603
Jakub Konopka <jakubkonopka@student.agh.edu.pl>
2024-01-19 15:03:04 +0100

    Bugfix do 1/1/1 genow
    nwm co to za kod tam byl, jakis dziwny, trzeba to obczaic

--- oolab/src/main/java/agh/ics/oop/model/AbstractGenes.java
+++ oolab/src/main/java/agh/ics/oop/model/AbstractGenes.java
@@ -86,8 +86,10 @@ public abstract class AbstractGenes {
 
         Collections.shuffle(genesPositions);
 
-        for(int i = 0; i < genesToSwitchAmount; i++){
-            genes.set(i, (genes.get(i) + random.nextInt(1, genesAmount)) % genesAmount);
+        for (int i = 0; i < genesToSwitchAmount; i++) {
+            int randomPosition = genesPositions.get(i);
+            //genes.set(randomPosition, (genes.get(randomPosition) + random.nextInt(1, genesAmount)) % genesAmount);
+            genes.set(randomPosition, (genes.get(randomPosition) + random.nextInt(7) + 1) % 8);
         }
     }
 
