6825aeaa91627ebbc3e94832f93629fb1e0968e3
Jakub Konopka <jakubkonopka@student.agh.edu.pl>
2024-01-19 22:21:06 +0100

    Update dodawania pustego folderu

--- oolab/src/main/java/agh/ics/oop/model/util/FolderGarbageCollector.java
+++ oolab/src/main/java/agh/ics/oop/model/util/FolderGarbageCollector.java
@@ -1,13 +1,24 @@
 package agh.ics.oop.model.util;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.NotDirectoryException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FolderGarbageCollector {
     private final static String path = "src/main/resources/stats";
 
-    public static void clearStatsDirectory() throws NotDirectoryException{
+    public static void clearStatsDirectory() throws NotDirectoryException {
         File directory = new File(path);
+
+        if (!directory.exists()) {
+            try {
+                createDirectoryIfNotExists(directory);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         if (directory.isDirectory()) {
             File[] files = directory.listFiles();
@@ -19,8 +30,13 @@ public class FolderGarbageCollector {
                     }
                 }
             }
-
+        } else {
+            throw new NotDirectoryException(path + " is not a directory!");
         }
-        else throw new NotDirectoryException(path + " is not a directory!");
+    }
+
+    private static void createDirectoryIfNotExists(File directory) throws IOException {
+        Path path = Paths.get(directory.getAbsolutePath());
+        java.nio.file.Files.createDirectories(path);
     }
 }
