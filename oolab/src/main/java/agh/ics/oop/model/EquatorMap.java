cc68569b8ee738ab84626961d5697f914d96ffcf
Jakub Konopka <jakubkonopka@student.agh.edu.pl>
2024-01-20 22:47:45 +0100

    fix do generowania jungli

--- oolab/src/main/java/agh/ics/oop/model/EquatorMap.java
+++ oolab/src/main/java/agh/ics/oop/model/EquatorMap.java
@@ -10,7 +10,7 @@ public class EquatorMap extends AbstractWorldMap{
 
     @Override
     protected void generateJungleTiles(){
-        int jungleTilesAmount = (int) (mapHeight*mapWidth*0.2);
+        int jungleTilesAmount = (int) (mapHeight * mapWidth * 0.2);
         int jungleTilesCounter = 0;
         double probabilityForRow = 1.0;
         int equator = mapHeight/2;
@@ -21,9 +21,9 @@ public class EquatorMap extends AbstractWorldMap{
         while(jungleTilesCounter < jungleTilesAmount){
             int x=0;
             while(x<mapWidth && jungleTilesCounter<jungleTilesAmount){
-                if(Math.random() < probabilityForRow){
+                if(Math.random() < probabilityForRow && tiles[equator + yModifier][x] != TileType.JUNG){
                     jungleTilesCounter++;
-                    tiles[equator+yModifier][x] = TileType.JUNG;
+                    tiles[equator + yModifier][x] = TileType.JUNG;
                 }
                 x++;
             }
@@ -40,7 +40,7 @@ public class EquatorMap extends AbstractWorldMap{
 
             if(!isInMap(equator, yModifier)){
                 yModifier = 0;
-                System.out.println(1);
+                probabilityForRow = 1.0;
             }
 
             probabilityForRow /= ((double) (mapHeight + 5) / mapHeight);
